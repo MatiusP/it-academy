debba746742ad6ca0b03bb0194a421e985c05715
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-11-05 20:12:01 +0300

    Additional Task2

--- src/by/it/protsko/jd01_05/AdditionalTask2.java
+++ src/by/it/protsko/jd01_05/AdditionalTask2.java
@@ -1,8 +1,29 @@
 package by.it.protsko.jd01_05;
 
+import static java.lang.Math.random;
+
 public class AdditionalTask2 {
 
     public static void main(String[] args) {
 
+        //step1: формирование матрицы
+        int[][] numberMatrix = createMatrix(5);
+        for (int[] row : numberMatrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
     }
+
+    private static int[][] createMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = -n + (int) (random() * ((n - (-n)) + 1));
+            }
+        }
+        return matrix;
+    }
+
 }
