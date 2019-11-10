48eb475f48402f82372c5bfd59fe988c204d2971
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-11-10 19:15:13 +0300

    Additional Task2

---
+++ src/by/it/protsko/jd01_06/TaskA2.java
@@ -0,0 +1,10 @@
+package by.it.protsko.jd01_06;
+
+import java.util.regex.Pattern;
+
+public class TaskA2 {
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]");
+    }
+}
