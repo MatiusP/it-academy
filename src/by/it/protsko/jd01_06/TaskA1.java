4008ea7b44fc20be57f3423227618abcec8d13a1
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-11-10 22:39:35 +0300

    Lesson6 - Homework

--- src/by/it/protsko/jd01_06/TaskA1.java
+++ src/by/it/protsko/jd01_06/TaskA1.java
@@ -4,11 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskA1 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         StringBuilder sb = new StringBuilder(Poem.text);
         Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]{4,}");
         Matcher matcher = pattern.matcher(sb);
+
         while (matcher.find()) {
             sb.setCharAt(matcher.start() + 3, '#');
             if (matcher.group().length() > 6) {
