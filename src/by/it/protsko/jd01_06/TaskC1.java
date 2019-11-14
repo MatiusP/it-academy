726ce991d75ebf6732a4efb9fbebed6960899b82
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-11-14 21:17:15 +0300

    Lesson 6, task C2

--- src/by/it/protsko/jd01_06/TaskC1.java
+++ src/by/it/protsko/jd01_06/TaskC1.java
@@ -1,18 +1,50 @@
 package by.it.protsko.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskC1 {
 
     public static void main(String[] args) {
-        int maxSentenceLength = 0;
+        String poem = Poem.text;
         String[] textArray = Poem.text.split("\\n");
+        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ,\\-!.]{1} ");
+        Matcher matcher;
+        StringBuilder text = new StringBuilder();
 
+        int maxSentenceLength = 0;
         for (String sentence : textArray) {
             if (sentence.length() > maxSentenceLength) {
                 maxSentenceLength = sentence.length();
             }
         }
-    }
-}
 
-//System.out.println(i + ": " +textArray[i]);
-//      System.out.println("Максимальная строка в "+maxSentenceLength +" символов");
+        for (int i = 0; i < textArray.length; i++) {
+            StringBuilder sentence = new StringBuilder(textArray[i]);
+
+            int needConcatSpace = maxSentenceLength - textArray[i].length();
+
+            if (sentence.length() < maxSentenceLength) {
+                matcher = pattern.matcher(sentence);
+                do {
+                    if (matcher.find()) {
+                        sentence.insert(matcher.start() + 1, ' ');
+                        needConcatSpace--;
+
+                    } else {
+                        matcher.reset();
+                    }
+                } while (needConcatSpace != 0);
+                text.append(sentence + "\n");
+                //text.append("\n");
+            } else {
+                continue;
+            }
+        }
+
+        poem = text.toString();
+        System.out.println(poem);
+    }
+
+
+}
