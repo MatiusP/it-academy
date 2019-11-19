e0d477e55fddd7af6e617e0e9e7d81cecfc25406
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-11-19 12:38:28 +0300

    Lesson 6 C-1

---
+++ src/by/it/protsko/calc/Patterns.java
@@ -0,0 +1,10 @@
+package by.it.protsko.calc;
+
+interface Patterns {
+
+    String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
+    String MATRIX = "\\{"+VECTOR+"(,"+VECTOR+")*}";
+    //String MATRIX ="\\{(\\{(-?[0-9]+(\\.[0-9]+)?)(,(-?[0-9]+(\\.[0-9]+)?))*})(,\\{(-?[0-9]+(\\.[0-9]+)?)(,(-?[0-9]+(\\.[0-9]+)?))*})*\\}";
+    String OPERATIONS = "[-+*/]";
+}
---
+++ src/by/it/protsko/calc/Printer.java
@@ -0,0 +1,8 @@
+package by.it.protsko.calc;
+
+class Printer {
+
+    void print(Var variable) {
+        System.out.println(variable);
+    }
+}
