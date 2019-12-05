0de1166f7000f064eee4c7c6b6e7114a5c3af3dc
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-12-05 20:22:23 +0300

    Lesson 16 tasks C

--- src/by/it/protsko/jd02_01/Market.java
+++ src/by/it/protsko/jd02_01/Market.java
@@ -9,56 +9,25 @@ class Market {
         System.out.println("Market opened!");
         List<Buyer> listBuyer = new ArrayList<>();
 
-        for (int time = 0; time <=0; time++) {
-
-            if (time == 0 || time == 60) {                    // начало каждой минуты работы магазина
-                while (Dispather.countBuyerInMarket <= 10) {
-                    int countBuyer = Helper.randomValue(10, 10);
-                    for (int j = 0; j <= countBuyer; j++) {
-                        Buyer buyer;
-                        if (++Dispather.countBuyer % 4 != 0) {
-                            buyer = new Buyer(Dispather.countBuyer);
-                        } else {
-                            buyer = new Buyer(Dispather.countBuyer, true);
-                        }
-                        listBuyer.add(buyer);
-                        buyer.start();
-                    }
-                    Thread.sleep(1000 / Dispather.speedProcess);
+        for (int time = 0; time <= 120; time++) {
+            int countBuyer = Helper.randomValue(0, 2);
+            for (int j = 1; j <= countBuyer; j++) {
+                Buyer buyer;
+                if (++Dispather.countBuyer % 4 != 0) {
+                    buyer = new Buyer(Dispather.countBuyer);
+                } else {
+                    buyer = new Buyer(Dispather.countBuyer, true);
                 }
+                listBuyer.add(buyer);
+                buyer.start();
             }
-
-
-
-
-
-
-//            if (time <= 30 || (time >= 60 && time <= 90)) {                     ///////////////new
-//                while (Dispather.countBuyerInMarket <) {
-//
-//                }
-//
-//
-//                int countBuyer = Helper.randomValue(0, 2);
-//
-//
-//                for (int j = 1; j <= countBuyer; j++) {
-//                    Buyer buyer;
-//                    if (++Dispather.countBuyer % 4 != 0) {
-//                        buyer = new Buyer(Dispather.countBuyer);
-//                    } else {
-//                        buyer = new Buyer(Dispather.countBuyer, true);
-//                    }
-//                    listBuyer.add(buyer);
-//                    buyer.start();
-//                }
-
-                //Thread.sleep(1000 / Dispather.speedProcess);
-            }
-
-//            for (Buyer buyer : listBuyer) {
-//                    buyer.join();
-//            }
-            System.out.println("Market closed!");
+            Thread.sleep(1000 / Dispather.speedProcess);
         }
+
+        for (Buyer buyer : listBuyer) {
+            buyer.join();
+        }
+        System.out.println("\nMarket closed!");
+        System.out.println("Count buyer in market=" +Dispather.countBuyerInMarket);
     }
+}
