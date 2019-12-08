d6707e833cd467046ad70d7ade341b7fbe998937
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-12-08 22:26:11 +0300

    Lesson 17 TaskA

---
+++ src/by/it/protsko/jd02_02/IBuyer.java
@@ -0,0 +1,10 @@
+package by.it.protsko.jd02_02;
+
+interface IBuyer {
+
+    void enterToMarket();
+
+    void chooseGoods() throws InterruptedException;
+
+    void goOut();
+}
---
+++ src/by/it/protsko/jd02_02/IUseBasket.java
@@ -0,0 +1,8 @@
+package by.it.protsko.jd02_02;
+
+public interface IUseBasket {
+
+    void takeBasket();
+
+    void putGoodsToBasket();
+}
