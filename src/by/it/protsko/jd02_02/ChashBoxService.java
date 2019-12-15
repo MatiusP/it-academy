60869705acd354f73ea9fbf4c6922a723370db41
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-12-15 22:00:40 +0300

    jd02_02

---
+++ src/by/it/protsko/jd02_02/ChashBoxService.java
@@ -0,0 +1,27 @@
+package by.it.protsko.jd02_02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChashBoxService extends Thread {
+    private List<Thread> cashierList = new ArrayList<>();
+    private int countCashier = 0;
+
+
+    @Override
+    public void run() {
+        while (!Dispather.marketClosed()) {
+            if (cashierList.size() <= 5 & (QueueBuyer.getQueueSize() > cashierList.size() * 5)) {
+                Cashier cashier = new Cashier(++countCashier);
+                Thread thread = new Thread(cashier);
+                cashierList.add(thread);
+                thread.start();
+            }
+            try {
+                Thread.sleep(Helper.randomValue(100, 500) / Dispather.speedProcess);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
